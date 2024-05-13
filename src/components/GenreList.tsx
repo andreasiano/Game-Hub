be9bfc2e53808eefac3303ff6dca042d06943945
AndreaPensieri <andrejazz2011@gmail.com>
2024-05-13 20:24:20 +0200

    Adding different categories

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,16 +1,22 @@
-import useGenres from "../hooks/useGenres";
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import { HStack, List, ListItem, Image, Button, Spinner } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
-export default function GenreList() {
-  const { data } = useGenres()
+interface Props {
+  onSelectGenre: (genre: Genre) => void
+}
+
+export default function GenreList({onSelectGenre}: Props) {
+  const { data, isLoading, error } = useGenres()
+  if(isLoading) return <Spinner/>
+  if(error) return null
   return (
     <List>
       {data.map((genre) => (
         <ListItem paddingY={'5px'} key={genre.id}>
           <HStack>
             <Image boxSize={'32px'} borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-            <Text fontSize={'lg'}>{genre.name}</Text>
+            <Button onClick={() => onSelectGenre(genre)} variant='link' fontSize={'lg'}>{genre.name}</Button>
           </HStack>
         </ListItem>
       ))}
